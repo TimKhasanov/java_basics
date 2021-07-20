3ab029daa994f95b0c9b2b25ad60d80cfa00a5b9
User <tim.hasanov24@gmail.com>
2021-07-20 19:56:18 +0300

    elevator class created

--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -2,8 +2,14 @@ package ru.skillbox;
 
 public class Elevator {
     private int currentFloor = 1;
-    private int minFloor = -3;
-    private int maxFloor = 26;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.maxFloor = maxFloor;
+        this.minFloor = minFloor;
+    }
+
 
     public void moveUp() {
         currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor;
@@ -18,28 +24,26 @@ public class Elevator {
     }
 
     public void move(int floor) {
-        if (floor > getCurrentFloor() && floor < maxFloor) {
-            System.out.println("Лифт двигается вверх:");
+        if (floor > getCurrentFloor() && floor <= maxFloor) {
+            System.out.println("Лифт движется вверх:");
             for (int a = getCurrentFloor(); a < floor; a++) {
-            if (getCurrentFloor() == floor){
-                break;
-            }
-            System.out.println(getCurrentFloor());
-            moveUp();
-            }
-
-        }
-        else if (floor < getCurrentFloor() && floor >= minFloor) {
-            System.out.println("Лифт двигается вниз:");
-            for (int a = getCurrentFloor(); a > floor; a--){
-                if (getCurrentFloor() == floor) {
+                if (getCurrentFloor() == floor){
                     break;
                 }
+                moveUp();
                 System.out.println(getCurrentFloor());
+            }
+
+        }
+        else if (floor < getCurrentFloor() && floor >= minFloor){
+            System.out.println("Лифт движется вниз:");
+            for (int a = getCurrentFloor(); a > floor; a++){
+                if (getCurrentFloor() == floor){
+                    break;
+                }
                 moveDown();
+                System.out.println(getCurrentFloor());
             }
         }
-
-
     }
 }
--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        Elevator elevator = new Elevator();
+        Elevator elevator = new Elevator(-3, 26);
         while (true){
             System.out.print("Введите номер этажа :");
-            int flor = new Scanner(System.in).nextInt();
-            elevator.move(flor);
+            int floor = new Scanner(System.in).nextInt();
+            elevator.move(floor);
         }
     }
 }
