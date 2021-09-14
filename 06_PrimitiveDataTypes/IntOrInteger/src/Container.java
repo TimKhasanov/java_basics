243e32e01578f46d78961f743b384bc16b2f9607
Тимур Хасанов <tim.hasanov24@gmail.com>
2021-09-14 10:53:48 +0300

    updated main method

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,5 +1,5 @@
 public class Container {
-    private Integer count = 0;
+    private int count;
 
     public void addCount(int value) {
         count = count + value;
--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
-        container.addCount(5672);
         System.out.println(container.getCount());
+
+        for (int i = 1040; i <= 1105; i++){
+            char c = (char) i;
+            System.out.println(i + "-" + c);
+        }
+
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
