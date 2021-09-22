fadb60f61270665837c3b93bf2e87a94df4b184d
Тимур Хасанов <tim.hasanov24@gmail.com>
2021-09-22 11:54:59 +0300

    generated code in main method

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,14 +1,39 @@
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int container = 0;
+        int truck = 0;
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество ящиков :");
         String boxes = scanner.nextLine();
+        int box = Integer.parseInt(boxes);
+        if (box > 0) {
+            container++;
+            truck++;
+            System.out.println("Грузовик: " + truck);
+            System.out.println("\tКонтейнер: " + container);
+            for (int i = 1; i <= box; i++) {
+                System.out.println("\t\tЯщик: " + i);
+                if (i % (12 * 27) == 0) {
+                    truck += 1;
+                    System.out.println("Грузовик: " + truck);
+                }
+                if (i % 27 == 0) {
+                    container += 1;
+                    System.out.println("\tКонтейнер: " + container);
+                }
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+            }
+            System.out.println("Необходимо:\nГрузовиков - " + truck + "шт." + "\nКонтейнеров - " + container + "шт.");
+
+        }
+    }
+    // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+    // пример вывода при вводе 2
+    // для отступа используйте табуляцию - \t
 
         /*
         Грузовик: 1
@@ -19,6 +44,4 @@ public class Main {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
-    }
-
 }
