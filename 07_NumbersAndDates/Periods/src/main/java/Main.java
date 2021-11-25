d0078a84bb2b76a6e10b344c09a4ffcefd7c2b84
Тимур Хасанов <tim.hasanov24@gmail.com>
2021-11-25 10:57:45 +0300

    A code is written that calculates the amount of earnings

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,20 @@
+package main.java;
+
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
 
   public static void main(String[] args) {
+    LocalDate birthday = LocalDate.of(1970,1,1);
+    System.out.println(getPeriodFromBirthday(birthday));
 
   }
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+  private static Period getPeriodFromBirthday(LocalDate birthday) {
+    LocalDate now = LocalDate.now();
+    return Period.between(birthday, now);
+
   }
 
 }
