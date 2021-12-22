243817aa717a78fe8510dde39cbaa47f82847ed9
Тимур Хасанов <tim.hasanov24@gmail.com>
2021-12-22 10:08:33 +0300

    Completed method for homework 9.9, giving 2

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String name = input;
+            int space = 0;
+            for (int i = 0; i < name.length(); i++) {
+                if (name.charAt(i) == ' ') {
+                    i++;
+                }
+
+            }
+            for (int i = 0; i < name.length(); i++) {
+                if (name.charAt(i) >= '0' && name.charAt(i) <= '9' || space > 2 || space == 0) {
+                    System.out.println("Введенная строка не является ФИО");
+                    break;
+                } else {
+                    System.out.println(
+                            "Фамилия: " + name.substring(0, name.indexOf(' ')) + System.lineSeparator() +
+                                    "Имя:" + name.substring(name.indexOf(' '), name.lastIndexOf(' ')) + System.lineSeparator() +
+                                    "Отчество:" + name.substring(name.lastIndexOf(' ')));
+                    break;
+
+                }
+
+            }
+
+
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        }
     }
-  }
 
 }
