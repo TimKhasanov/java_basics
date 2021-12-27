7d7e346df9bed49f2969edeebbe8ca2cba22c6e4
Тимур Хасанов <tim.hasanov24@gmail.com>
2021-12-27 09:57:51 +0300

    Main method changed

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -13,30 +13,27 @@ public class Main {
             String name = input;
             int space = 0;
             for (int i = 0; i < name.length(); i++) {
-                if (name.charAt(i) == ' ') {
-                    i++;
+                char symbol = name.charAt(i);
+                if (symbol == ' ') {
+                    space++;
+                    continue;
                 }
-
-            }
-            for (int i = 0; i < name.length(); i++) {
-                if (name.charAt(i) >= '0' && name.charAt(i) <= '9' || space > 2 || space == 0) {
-                    System.out.println("Введенная строка не является ФИО");
+                if (Character.UnicodeBlock.of(symbol) != Character.UnicodeBlock.CYRILLIC && symbol != '-') {
+                    space = 0;
                     break;
-                } else {
-                    System.out.println(
-                            "Фамилия: " + name.substring(0, name.indexOf(' ')) + System.lineSeparator() +
-                                    "Имя:" + name.substring(name.indexOf(' '), name.lastIndexOf(' ')) + System.lineSeparator() +
-                                    "Отчество:" + name.substring(name.lastIndexOf(' ')));
-                    break;
-
                 }
-
+            }
+            if (space != 2) {
+                System.out.println("Введенная строка не является ФИО");
+            } else {
+                System.out.println(
+                        "Фамилия: " + name.substring(0, name.indexOf(' ')) + System.lineSeparator() +
+                                "Имя:" + name.substring(name.indexOf(' '), name.lastIndexOf(' ')) + System.lineSeparator() +
+                                "Отчество:" + name.substring(name.lastIndexOf(' ')));
             }
 
-
-            //TODO:напишите ваш код тут, результат вывести в консоль.
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
         }
-    }
 
+    }
 }
+
