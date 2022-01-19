b59793f319e2e532cfa14659bd068fb8e14bc49b
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-01-19 10:18:59 +0300

    Main method changed

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-
+  static int sum = 0;
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    String text = scanner.nextLine();
+    System.out.println(calculateSalarySum(text));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+    Pattern pattern = Pattern.compile("([0-9]+) руб");
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      String result = text.substring(start, end);
+      result = result.replaceAll("([^0-9]+)", "").trim();
+      int i = Integer.parseInt(result);
+      sum += i;
+
+
+    }
+    return sum;
+
   }
 
-}
+
+  }
+
