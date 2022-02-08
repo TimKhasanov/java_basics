f34d561d2835e6eb949b26f2dce164d6ca6d6671
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-02-08 22:41:47 +0300

    Added class FullNameFormatterRegExp

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String text = input;
+            String[] words = text.split(" ");
+            String regex = "[^А-яёЁ\\-]+";
+            String b = "";
+            for (int i = 0; i < words.length; i++) {
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(text);
+                if (matcher.find()) {
+                    b += words[i] + " ";
+                }
+                if (b != regex) {
+                    System.out.println("Введенная строка не является ФИО");
+
+
+                }
+                else {
+                    b = b.trim();
+                    System.out.println(words[i]);
+                }
+
+
+
+            }
+
+        }
     }
-  }
-
 }
