d2a44ec48f1232c9842a7dbdb71ce74a35cb8b31
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-02-10 23:44:27 +0300

    Class task completed FullNameFormatterRegExp

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,29 +14,13 @@ public class Main {
                 break;
             }
             String text = input;
-            String[] words = text.split(" ");
-            String regex = "[^А-яёЁ\\-]+";
-            String b = "";
-            for (int i = 0; i < words.length; i++) {
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(text);
-                if (matcher.find()) {
-                    b += words[i] + " ";
-                }
-                if (b != regex) {
-                    System.out.println("Введенная строка не является ФИО");
-
-
-                }
-                else {
-                    b = b.trim();
-                    System.out.println(words[i]);
-                }
-
-
-
+            String regex = "(\\W+\\s){2}\\W+";
+            if (text.matches(regex)) {
+                String[] word = text.split(" ");
+                System.out.println("Фамилия: " + word[0] + "\n" + "Имя: " + word[1] + "\n" + "Отчество: " + word[2]);
+            } else {
+                System.out.println("Введенная строка не является ФИО");
             }
-
         }
     }
 }
