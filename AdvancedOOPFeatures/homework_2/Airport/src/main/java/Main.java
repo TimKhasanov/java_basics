c4aaeff1fe96cd55837861ea86ebf131c5fe1eba
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-06-03 18:27:47 +0300

    Updated the main method

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,30 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Date dateNow = new Date();
+        Date date = new Date();
+        date.setHours(date.getHours()+2);
+
+        List<Flight> list = airport.getTerminals().stream().
+                flatMap(terminal -> terminal.getFlights().stream())
+                .filter(time -> time.getDate().after(dateNow))
+                .filter(time -> time.getDate().before(date))
+                .filter(i -> i.getType() == Flight.Type.DEPARTURE)
+                .collect(Collectors.toList());
+
+        return list;
     }
 
 }
