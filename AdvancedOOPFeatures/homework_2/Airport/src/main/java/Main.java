0f893d1559afda6cdd031a60de01556dde35ca19
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-06-04 19:55:47 +0300

    Updated the main method

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -14,17 +16,18 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         Date dateNow = new Date();
-        Date date = new Date();
-        date.setHours(date.getHours()+2);
+        Date input = new Date();
+        LocalDateTime date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().plusHours(2);
+        Date out = Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+
 
         List<Flight> list = airport.getTerminals().stream().
                 flatMap(terminal -> terminal.getFlights().stream())
                 .filter(time -> time.getDate().after(dateNow))
-                .filter(time -> time.getDate().before(date))
+                .filter(time -> time.getDate().before(out))
                 .filter(type -> type.getType() == Flight.Type.DEPARTURE)
                 .collect(Collectors.toList());
 
         return list;
     }
-
 }
