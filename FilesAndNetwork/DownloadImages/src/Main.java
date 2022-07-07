3dc62a163a9bd605699ec0075e5c0d41d3a0db2e
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-07-07 13:00:30 +0300

    changed the main method

---
+++ FilesAndNetwork/DownloadImages/src/Main.java
@@ -0,0 +1,48 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.*;
+import java.net.URL;
+
+
+public class Main {
+    private static  File file = new File("images");
+
+
+    public static void main(String[] args) {
+
+        try {
+
+            Document document = Jsoup.connect("https://lenta.ru/").get();
+            Elements elements = document.select("img");
+            for(Element elements1 : elements){
+                String srs = elements1.attr("src");
+                getImages(srs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    private static void getImages(String src){
+        String fileName = src.substring(src.lastIndexOf("/") + 1);
+
+        try {
+            URL url = new URL(src);
+            InputStream in = url.openStream();
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(file + fileName));
+            for (int b; (b = in.read()) != -1; ) {
+                out.write(b);
+            }
+            out.close();
+            in.close();
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
