87659ad18af612e0571ba55bb89d800a7a4285ac
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-07-10 19:10:11 +0300

    Changes have been made to the FileUtils class

--- FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
+++ FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
@@ -3,21 +3,21 @@ import java.io.File;
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        long sum = 0;
+        long fileSize = 0;
         try {
             File file = new File(path);
             if (file.isDirectory()) {
                 for (File file1 : file.listFiles()) {
                     if (file1.isFile()) {
-                        sum += file1.length();
+                        fileSize += file1.length();
                     } else {
-                        sum += calculateFolderSize(file1.getPath());
+                        fileSize += calculateFolderSize(file1.getPath());
                     }
                 }
             }
         } catch (Exception exception) {
             exception.printStackTrace();
         }
-        return sum;
+        return fileSize;
     }
 }
