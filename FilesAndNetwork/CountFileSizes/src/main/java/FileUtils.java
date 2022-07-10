9ac21aa6af8081299c616ff53f784741f7bd11d9
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-07-10 19:05:08 +0300

    The task was completed in the CountFileSizes project

--- FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
+++ FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
@@ -1,6 +1,23 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long sum = 0;
+        try {
+            File file = new File(path);
+            if (file.isDirectory()) {
+                for (File file1 : file.listFiles()) {
+                    if (file1.isFile()) {
+                        sum += file1.length();
+                    } else {
+                        sum += calculateFolderSize(file1.getPath());
+                    }
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return sum;
     }
 }
--- FilesAndNetwork/CountFileSizes/src/main/java/Main.java
+++ FilesAndNetwork/CountFileSizes/src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        for (;;) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите путь до папки: ");
+            String path = scanner.nextLine();
+            System.out.println("Размер папки: " + path + " составляет " + getPathSize(FileUtils.calculateFolderSize(path)));
+        }
+    }
 
+    public static String getPathSize(long size) {
+        String result = "";
+        double kb = 1024;
+        double mb = 1024 * 1024;
+        double gb = (1024 * 1024) * 1024;
+
+        if (size < kb) {
+            result += size + "b";
+        } else if (size >= kb && size <= mb) {
+            result += String.format("%.1f", size / kb) + " Kb";
+        } else if (size >= mb && size <= gb) {
+            result += String.format("%.1f", size / mb) + " Mg";
+        } else if (size >= gb) {
+            result += String.format("%.1f", size / gb) + " Gb";
+        }
+        return result;
     }
 }
